81a6e0a6fbc5dae8f6ede2ef33cb866aa4401747
Dev-Yesung <yeasung67@gmail.com>
2022-10-05 13:56:10 +0900

    version push

---
+++ src/main/java/uplord/uplordapi/UpLordApiApplication.java
@@ -0,0 +1,13 @@
+package uplord.uplordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpLordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpLordApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uplord/uplordapi/UpLordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uplord.uplordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpLordApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
