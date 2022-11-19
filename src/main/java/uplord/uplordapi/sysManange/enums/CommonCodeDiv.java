e40b2e0abcd09d33f73ba067a496fd2cc2497f91
황성태 <tjdxo11933@naver.com>
2022-11-20 00:24:20 +0900

    [시스템관리] 쿼리 수정

---
+++ src/main/java/uplord/uplordapi/sysManange/enums/CommonCodeDiv.java
@@ -0,0 +1,28 @@
+package uplord.uplordapi.sysManange.enums;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum CommonCodeDiv {
+    SYSTEM_CODE_DIV("S"),
+    USER_CODE_DIV("U");
+
+    private final String code;
+
+    CommonCodeDiv(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    private static final Map<String, CommonCodeDiv> BY_CODE =
+            Stream.of(CommonCodeDiv.values()).collect(Collectors.toMap(CommonCodeDiv::getCode, Function.identity()));
+
+    public static CommonCodeDiv valueOfCode(String code) {
+        return BY_CODE.get(code);
+    }
+}
