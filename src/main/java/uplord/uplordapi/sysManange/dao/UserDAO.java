d58a51a6f6fa16ce604fef11db7759d0d970a8eb
황성태 <tjdxo11933@naver.com>
2022-11-21 01:26:35 +0900

    사용자관리 Api

--- src/main/java/uplord/uplordapi/sysManange/dao/UserDAO.java
+++ src/main/java/uplord/uplordapi/sysManange/dao/UserDAO.java
@@ -1,9 +1,13 @@
 package uplord.uplordapi.sysManange.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import uplord.uplordapi.sysManange.dto.UserDTO;
 
 import java.util.List;
 
+@Mapper
+@Repository
 public interface UserDAO {
     List<UserDTO> findList(UserDTO param);
 
