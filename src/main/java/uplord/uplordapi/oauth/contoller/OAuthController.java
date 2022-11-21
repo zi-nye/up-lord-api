432e32afb9cb3a748af7b5a1b3334d984c3e0c81
Minsung-1 <orange@justbuild.kr>
2022-11-21 23:06:01 +0900

    restTemplate 사용

--- src/main/java/uplord/uplordapi/oauth/contoller/OAuthController.java
+++ src/main/java/uplord/uplordapi/oauth/contoller/OAuthController.java
@@ -1,9 +1,12 @@
 package uplord.uplordapi.oauth.contoller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uplord.uplordapi.oauth.service.OAuthService;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,24 +18,32 @@ public class OAuthController {
 
     @ResponseBody
     @PostMapping("/kakao")
-    public String getAuthorizationCode(@RequestBody Map<String, String> body){
-        // TODO 서비스에서 하도록 할 것.
-            // 인증코드 가지고 토큰 받기 😃
-            // 토큰 받은 것으로 유저 조회하기
-            // 카카오톡 유저 조회 API 조회
-            // 조회한 데이터를 참고하여 토큰 발급하기
-                // 이미 입력된 유저라면, 그냥 토큰 주기
-                // 아니라면, 신규로 삽입하고 토큰 주기
+    public ResponseEntity<Map<String, String>> getAuthorizationCode(@RequestBody Map<String, String> body){
 
-        //TODO
-        // API 요청하는 모듈 만들기: common 패키지 아래 패키지 하나 더 만들고 그 아래에 만들기
-        // MyBatis 생성
+        Map<String, String> result = new HashMap<>();
+        try {
+            String authCode = body.get("code");
 
-        String authCode = body.get("code");
+            String access_Token = service.getKakaoAccessToken(authCode);
+            String token = service.createKakaoUser(access_Token);
 
-        String access_Token = service.getKakaoAccessToken(authCode);
-        String token = service.createKakaoUser(access_Token);
+            result.put("token", token);
 
-        return token;
+            return ResponseEntity.ok(result);
+
+
+        }catch (IOException ioe){
+            String msg = "카카오톡 API 통신 중 오류가 발생했습니다.";
+            result.put("msg", msg);
+            return ResponseEntity.internalServerError()
+                    .body(result);
+
+
+        }catch (Exception e) {
+            String msg = e.getMessage();
+            result.put("msg", msg);
+            return ResponseEntity.internalServerError()
+                    .body(result);
+        }
     }
 }
