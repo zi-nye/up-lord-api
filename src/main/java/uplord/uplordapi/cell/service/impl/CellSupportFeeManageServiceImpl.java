498bc576181c6d06123e5a3d3e970b068d323752
황성태 <tjdxo11933@naver.com>
2023-01-31 23:04:53 +0900

    오류 부분 수정

--- src/main/java/uplord/uplordapi/cell/service/impl/CellSupportFeeManageServiceImpl.java
+++ src/main/java/uplord/uplordapi/cell/service/impl/CellSupportFeeManageServiceImpl.java
@@ -1,6 +1,7 @@
 package uplord.uplordapi.cell.service.impl;
 
+import org.springframework.stereotype.Service;
 import uplord.uplordapi.cell.service.CellSupportFeeManageService;
-
+@Service
 public class CellSupportFeeManageServiceImpl implements CellSupportFeeManageService {
 }
