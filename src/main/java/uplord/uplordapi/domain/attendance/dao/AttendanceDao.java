7dbf4ed2468d21c9697116c509454e94bec7f145
황성태 <tjdxo11933@naver.com>
2023-05-01 03:14:20 +0900

    mapper error

--- src/main/java/uplord/uplordapi/domain/attendance/dao/AttendanceDao.java
+++ src/main/java/uplord/uplordapi/domain/attendance/dao/AttendanceDao.java
@@ -1,11 +1,12 @@
 package uplord.uplordapi.domain.attendance.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import uplord.uplordapi.domain.attendance.dto.AttendanceDto;
 import uplord.uplordapi.domain.attendance.dto.RegisterAttendanceCommand;
 import uplord.uplordapi.domain.attendance.entity.Attendance;
 
 import java.util.List;
-
+@Mapper
 public interface AttendanceDao {
     Long createAttendance(Attendance registerAttendanceCommand);
 
