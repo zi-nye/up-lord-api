43cf08aa3a46479f5131f64d9d60d07956ddf1f5
ysng_ysng <yeasung67@gmail.com>
2023-05-18 16:13:42 +0900

    cd(application.yml): Fix start.sh to deploy continuously
    Fix start.sh to deploy continuously

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app
-PROJECT_NAME=up-lord-api
+PROJECT_NAME=Up-Lord-API-0.0.1-SNAPSHOT
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
@@ -25,4 +25,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
   -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
   -Dspring.profiles.active=$IDLE_PROFILE \
-  $JAR_NAME >$REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
