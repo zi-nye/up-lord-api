04873ebc11b335ba7398ec7078c101edd601804a
ysng_ysng <yeasung67@gmail.com>
2023-05-20 00:34:02 +0900

    fix: Fix switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
   echo "> 전환할 port 번호 : $IDLE_PORT"
   echo "> port를 전환합니다."
-  echo "> set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> NGINX Reload"
   sudo service nginx reload
 }
